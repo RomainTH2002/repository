8240a3da84b26f9d9ee51bc9e3f6fe806e7750cc
Romain <thiriot2002@gmail.com>
2022-03-28 09:24:07 +0200

    did

--- ProjetClasse/src/application/Main.java
+++ ProjetClasse/src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("appli");
+			primaryStage.setTitle("appli test");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
